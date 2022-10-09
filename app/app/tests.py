4243a5d84e026e3243f2eb78e1aaf1b0fef70df7
Sonny Svensson <sonny-svensson@hotmail.com>
2022-10-09 13:28:17 +0200

    Added some tests and installed psykopg2 for db connection to postgres in Dockerfile

---
+++ app/app/tests.py
@@ -0,0 +1,18 @@
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_numbers(self):
+        
+        res = calc.add(5, 5)
+
+        self.assertEqual(res, 10)
+
+    def test_subtract_numbers(self):
+
+        res = calc.subtract(5, 3)
+
+        self.assertEqual(res, 2)
+
